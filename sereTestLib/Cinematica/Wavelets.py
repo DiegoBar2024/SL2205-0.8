b8bf0f6fc82350e3dd77b114e16fa11759263c98
DiegoBar2024 <diego.barboza@correo.ucu.edu.uy>
2024-11-23 01:12:40 -0300

    Agrego archivo para graficar escalogramas

--- sereTestLib/Cinematica/Wavelets.py
+++ sereTestLib/Cinematica/Wavelets.py
@@ -4,6 +4,9 @@ import seaborn as sns
 import numpy as np
 import sys
 import pywt
+import obspy
+from obspy.imaging.cm import obspy_sequential
+from obspy.signal.tf_misfit import cwt
 
 # Cantidad de muestras
 N = 600
@@ -15,31 +18,18 @@ T = 1.0 / 1500.0
 x = np.linspace(0.0, N * T, N, endpoint = False)
 
 ## Función
-y = np.cos(50.0 * 2.0 * np.pi * x)
+y = np.cos(50.0 * 2.0 * np.pi * x) + 2 * np.cos(100.0 * 2.0 * np.pi * x)
 
+## Ploteo de Escalograma
 coef, scales_freq = pywt.cwt(data = y, scales = [1, 10], wavelet = 'cmor1.5-1', sampling_period = T)
 
 coef = np.abs(coef[:-1, :-1])
 
-fig, axs = plt.subplots(2, 1)
-pcm = axs[0].pcolormesh(x, scales_freq, coef)
-axs[0].set_yscale("log")
-axs[0].set_xlabel("Time (s)")
-axs[0].set_ylabel("Frequency (Hz)")
-axs[0].set_title("Continuous Wavelet Transform (Scaleogram)")
-fig.colorbar(pcm, ax=axs[0])
+fig, axs = plt.subplots(1, 1)
+pcm = axs.pcolormesh(x, scales_freq, coef)
+axs.set_yscale("log")
+axs.set_xlabel("Time (s)")
+axs.set_ylabel("Frequency (Hz)")
+axs.set_title("Continuous Wavelet Transform (Scaleogram)")
+fig.colorbar(pcm, ax=axs)
 plt.show()
-
-# def plot_wavelet(time, data, wavelet, title, ax):
-#     widths = np.geomspace(1, 1024, num=75)
-#     cwtmatr, freqs = pywt.cwt(
-#         data, widths, wavelet, sampling_period=np.diff(time).mean()
-#     )
-#     cwtmatr = np.abs(cwtmatr[:-1, :-1])
-#     pcm = ax.pcolormesh(time, freqs, cwtmatr)
-#     ax.set_yscale("log")
-#     ax.set_xlabel("Time (s)")
-#     ax.set_ylabel("Frequency (Hz)")
-#     ax.set_title(title)
-#     plt.colorbar(pcm, ax=ax)
-#     return ax
