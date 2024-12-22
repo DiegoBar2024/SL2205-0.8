6c9cf4de9d3d89acf618587417283bf99946bd52
DiegoBar2024 <diego.barboza@correo.ucu.edu.uy>
2024-12-21 21:39:34 -0300

    Modifico calculo y graficación de escalogramas

--- sereTestLib/Wavelets/Escalogramas.py
+++ sereTestLib/Wavelets/Escalogramas.py
@@ -2,17 +2,19 @@
 
 import sys
 sys.path.append('C:/Yo/Tesis/SL2205-0.8/SL2205-0.8/sereTestLib/Cinematica')
-from Segmentacion import *
+from SegmentacionM1 import *
 
 ## --------------------------------------------- PARÁMETROS --------------------------------------------
 
 ## Escalas de las wavelets a utilizar
-escalas = [8, 136]
+escalas = np.arange(10, 150, 1)
 
-## Tipo de wavelet a utilizar
+## Tipo de wavelet a utilizar. En principio Morlet Wavelet Compleja
+## Parámetro B (Ancho de banda): 1.5 Hz
+## Parámetro C (Frecuencia Central): 1 Hz
 wavelet = 'cmor1.5-1'
 
-## ------------------------------------- SEGMENTACIÓN (PEAK FINDING) -----------------------------------
+## ------------------------------------------- SEGMENTACIÓN --------------------------------------------
 
 ## Creo una matriz donde se van a guardar los segmentos de las señales de acelerómetros y giroscopios según los pasos detectados
 ## La indexación de <<matriz_segmentada_pf>> en [Dimension1, Dimension2, Dimension3] se indica del siguiente modo:
@@ -21,44 +23,41 @@ wavelet = 'cmor1.5-1'
 ##  iii) Dimension3: Indica el instante temporal
 matriz_segmentada_pf = np.zeros(len(pasos), dtype = object)
 
+## Defino una variable en la cual voy a guardar el tiempo de extensión expresado en cantidad de muestras
+extension = 800
+
+## Creo un vector en donde voy guardando las extensiones en muestras correspondientes a cada paso
+extensiones_pasos = []
+
 ## Ahora itero para cada uno de los pasos que tengo segmentados
 for i in range (len(pasos)):
 
+    ## En caso de que la extensión sea mayor al tiempo que tengo, lo seteo en el limite
+    if extension > pasos[i]['IC'][0]:
+
+        ## Actualizo el valor de la extensión a realizar
+        extension = pasos[i]['IC'][0]
+
     ## Filas de <<matriz>>: Son cada uno de los 6 canales que tengo. Es decir señales de acelerómetros y giroscopios
     ## Columnas de <<matriz>>: Son cada uno de los instantes temporales que componen el paso detectado entre dos ICs
-    matriz = np.array([acel[:,0][pasos[i]['IC'][0] : pasos[i]['IC'][1]],
-                            acel[:,1][pasos[i]['IC'][0] : pasos[i]['IC'][1]],
-                            acel[:,2][pasos[i]['IC'][0] : pasos[i]['IC'][1]],
-                            gyro[:,0][pasos[i]['IC'][0] : pasos[i]['IC'][1]],
-                            gyro[:,1][pasos[i]['IC'][0] : pasos[i]['IC'][1]],
-                            gyro[:,2][pasos[i]['IC'][0] : pasos[i]['IC'][1]]])
+    ## Para mitigar los efectos de borde se le agrega una extensión en ambos lados del intervalo [IC[i] - e, IC[i+1] + e]
+    matriz = np.array([ acel[:,0][(pasos[i]['IC'][0] - extension) : (pasos[i]['IC'][1] + extension)],
+                        acel[:,1][(pasos[i]['IC'][0] - extension) : (pasos[i]['IC'][1] + extension)],
+                        acel[:,2][(pasos[i]['IC'][0] - extension) : (pasos[i]['IC'][1] + extension)],
+                        gyro[:,0][(pasos[i]['IC'][0] - extension) : (pasos[i]['IC'][1] + extension)],
+                        gyro[:,1][(pasos[i]['IC'][0] - extension) : (pasos[i]['IC'][1] + extension)],
+                        gyro[:,2][(pasos[i]['IC'][0] - extension) : (pasos[i]['IC'][1] + extension)] ])
 
     ## Agrego la matriz al tensor de datos segmentados
     matriz_segmentada_pf[i] = matriz
 
-## ------------------------------------- SEGMENTACIÓN (ZERO CROSSING) ----------------------------------
+    ## Guardo el valor de la extensión para el i-ésimo paso
+    extensiones_pasos.append(extension)
 
-## Creo una matriz donde se van a guardar los segmentos de las señales de acelerómetros y giroscopios según los pasos detectados
-## La indexación de <<matriz_segmentada_zc>> en [Dimension1, Dimension2, Dimension3] se indica del siguiente modo:
-##  i)   Dimension1: Indica el número de paso
-##  ii)  Dimension2: Indica el canal
-##  iii) Dimension3: Indica el instante temporal
-matriz_segmentada_zc = np.zeros(len(pasos_zc), dtype = object)
+    ## Vuelvo a actualizar el valor de la extension para que vuelva a iterar el ciclo
+    extension = 800
 
-## Ahora itero para cada uno de los pasos que tengo segmentados
-for i in range (len(pasos_zc)):
-
-    ## Filas de <<matriz>>: Son cada uno de los 6 canales que tengo. Es decir señales de acelerómetros y giroscopios
-    ## Columnas de <<matriz>>: Son cada uno de los instantes temporales que componen el paso detectado entre dos ICs
-    matriz = np.array([acel[:,0][pasos_zc[i]['IC'][0] : pasos_zc[i]['IC'][1]],
-                            acel[:,1][pasos_zc[i]['IC'][0] : pasos_zc[i]['IC'][1]],
-                            acel[:,2][pasos_zc[i]['IC'][0] : pasos_zc[i]['IC'][1]],
-                            gyro[:,0][pasos_zc[i]['IC'][0] : pasos_zc[i]['IC'][1]],
-                            gyro[:,1][pasos_zc[i]['IC'][0] : pasos_zc[i]['IC'][1]],
-                            gyro[:,2][pasos_zc[i]['IC'][0] : pasos_zc[i]['IC'][1]]])
-
-    ## Agrego la matriz al tensor de datos segmentados
-    matriz_segmentada_zc[i] = matriz
+print(extensiones_pasos)
 
 ## -------------------------------------- ESCALOGRAMAS (PEAK FINDING) ----------------------------------
 
@@ -92,47 +91,22 @@ for i in range (len(pasos)):
     coef6, scales_freq = pywt.cwt(data = matriz_segmentada_pf[i][5], scales = escalas, wavelet = wavelet, sampling_period = periodoMuestreo)
 
     ## Agrego al tensor de escalogramas una pila con los 6 coeficientes
-    matriz_escalogramas_pf[i] = np.array([coef1, coef2, coef3, coef4, coef5, coef6])
+    matriz_escalogramas_pf[i] = np.array([  coef1[:,extensiones_pasos[i]: coef1.shape[1] - extensiones_pasos[i]], 
+                                            coef2[:,extensiones_pasos[i]: coef2.shape[1] - extensiones_pasos[i]],
+                                            coef3[:,extensiones_pasos[i]: coef3.shape[1] - extensiones_pasos[i]], 
+                                            coef4[:,extensiones_pasos[i]: coef4.shape[1] - extensiones_pasos[i]], 
+                                            coef5[:,extensiones_pasos[i]: coef5.shape[1] - extensiones_pasos[i]], 
+                                            coef6[:,extensiones_pasos[i]: coef6.shape[1] - extensiones_pasos[i]]   ])
 
-    # fig, axs = plt.subplots(1, 1)
-    # pcm = axs.pcolormesh(np.abs(coef2[:-1, :-1]))
-    # axs.set_yscale("log")
-    # axs.set_xlabel("Time (s)")
-    # axs.set_ylabel("Frequency (Hz)")
-    # axs.set_title("Continuous Wavelet Transform (Scaleogram)")
-    # fig.colorbar(pcm, ax=axs)
-    # plt.show()
+    data = np.abs(coef3[:,extensiones_pasos[i]: coef3.shape[1] - extensiones_pasos[i]]) ** 2
+    cmap = plt.get_cmap('jet', 256)
+    fig = plt.figure(figsize=(5,5))
+    ax = fig.add_subplot(111)
+    t = np.arange(coef3[:,extensiones_pasos[i]: coef3.shape[1] - extensiones_pasos[i]].shape[1]) * periodoMuestreo
 
-## -------------------------------------- ESCALOGRAMAS (ZERO CROSSING) ---------------------------------
+    ax.pcolormesh(t, scales_freq, data, cmap=cmap, vmin=data.min(), vmax=data.max(), shading='auto')
 
-## Creo una matriz en la cual para cada paso asocio un tensor tridimensional con las transformadas de wavelet
-## La indexación de <<matriz_escalogramas_zc>> en [Dimension1, Dimension2, Dimension3, Dimension4] se indica del siguiente modo:
-##  i)   Dimension1: Indica el número de paso
-##  ii)  Dimension2: Indica el canal
-##  iii) Dimension3: Indica la escala
-##  iv)  Dimension4: Indica el instante temporal
-matriz_escalogramas_zc = np.zeros(len(pasos), dtype = object)
+    plt.show()
 
-## Itero para cada uno de los pasos que tengo segmentados
-for i in range (len(pasos)):
-
-    ## Transformada de Wavelet de la aceleración en el eje x
-    coef1, scales_freq = pywt.cwt(data = matriz_segmentada_zc[i][0], scales = escalas, wavelet = wavelet, sampling_period = periodoMuestreo)
-
-    ## Transformada de Wavelet de la aceleración en el eje y
-    coef2, scales_freq = pywt.cwt(data = matriz_segmentada_zc[i][1], scales = escalas, wavelet = wavelet, sampling_period = periodoMuestreo)
-
-    ## Transformada de Wavelet de la aceleración en el eje z
-    coef3, scales_freq = pywt.cwt(data = matriz_segmentada_zc[i][2], scales = escalas, wavelet = wavelet, sampling_period = periodoMuestreo)
-
-    ## Transformada de Wavelet de la señal de giroscopio respecto del eje x
-    coef4, scales_freq = pywt.cwt(data = matriz_segmentada_zc[i][3], scales = escalas, wavelet = wavelet, sampling_period = periodoMuestreo)
-    
-    ## Transformada de Wavelet de la señal de giroscopio respecto del eje y
-    coef5, scales_freq = pywt.cwt(data = matriz_segmentada_zc[i][4], scales = escalas, wavelet = wavelet, sampling_period = periodoMuestreo)
-    
-    ## Transformada de Wavelet de la señal de giroscopio respecto del eje z
-    coef6, scales_freq = pywt.cwt(data = matriz_segmentada_zc[i][5], scales = escalas, wavelet = wavelet, sampling_period = periodoMuestreo)
-
-    ## Agrego al tensor de escalogramas una pila con los 6 coeficientes
-    matriz_escalogramas_zc[i] = np.array([coef1, coef2, coef3, coef4, coef5, coef6])
+    plt.plot(acel[:,2][pasos[i]['IC'][0] : pasos[i]['IC'][1]])
+    plt.show()
--- sereTestLib/Wavelets/Wavelets.py
+++ sereTestLib/Wavelets/Wavelets.py
@@ -3,6 +3,65 @@ import scaleogram as scg
 import seaborn as sns
 import numpy as np
 import pywt
+import math
+from scipy.integrate import cumulative_trapezoid
+
+def EnergiaSeñalTiempo(señal):
+    """
+    Función que me calcula la energía total de la señal de entrada en el dominio temporal
+    """
+
+    ## Creo una variable donde guardo la energía calculada
+    energia = 0
+
+    ## Itero para cada una de las muestras de la señal de entrada
+    for muestra in señal:
+
+        ## Actualizo el valor de la energía sumando el cuadrado de la muestra
+        energia += np.abs(muestra) ** 2
+
+    ## Retorno el valor de la energía
+    return energia
+
+def EnergiaCWT(CWT, escalas):
+    """
+    Función que calcula la energía total de los coeficientes de la CWT de una señal
+    """
+
+    ## Creo una variable donde guardo la energía de la CWT
+    energia_cwt = 0
+
+    ## Itero para cada una de las escalas de la CWT las cuales serán las filas de la matriz de coeficientes
+    for i in range (len(escalas)):
+
+        ## Itero para cada uno de los valores temporales los cuales serán las columnas de la matriz de coeficientes
+        for j in range (CWT.shape[1]):
+
+            ## Actualizo la variable donde guardo la energía de la wavelet
+            energia_cwt += (np.abs(CWT[i,j]) / escalas[i]) ** 2
+    
+    ## Imprimo el valor de la energía de la CWT
+    return energia_cwt
+
+def ConstanteAdmisibilidad(f_central, sigma):
+    """
+    Función que calcula la constante de admisibilidad de una Morlet Compleja con parámetros dados como entrada
+    """
+
+    ## Genero un vector de frecuencias arbitrariamente largo
+    w = np.arange(0.00000005, 1000, 0.01)
+
+    ## Genero un vector con el valor w0
+    w0 = 2 * math.pi * f_central * np.ones(w.shape)
+
+    ## Escribo el integrando en base al análsis teórico
+    integrando = (math.e ** ((- sigma * (w - w0) ** 2) / 2)) / w
+
+    ## Aplico la regla trapezoidal para integrar
+    Cg = cumulative_trapezoid(integrando, w, initial=0)
+
+    ## Retorno la constante de admisibilidad
+    return Cg
 
 # Cantidad de muestras
 N = 600
@@ -14,18 +73,24 @@ T = 1.0 / 1500.0
 x = np.linspace(0.0, N * T, N, endpoint = False)
 
 ## Función
-y = np.cos(50.0 * 2.0 * np.pi * x) + 2 * np.cos(100.0 * 2.0 * np.pi * x)
+y = 2 * np.cos(100.0 * 2.0 * np.pi * x)
+
+## Escalas de las wavelets
+escalas = np.arange(0.5, 400, 0.5)
 
 ## Ploteo de Escalograma
-coef, scales_freq = pywt.cwt(data = y, scales = [1, 10], wavelet = 'cmor1.5-1', sampling_period = T)
+coef, scales_freq = pywt.cwt(data = y, scales = escalas, wavelet = 'cmor1.5-1', sampling_period = T)
 
-coef = np.abs(coef[:-1, :-1])
+data = np.abs(coef) ** 2
+cmap = plt.get_cmap('jet', 256)
+fig = plt.figure(figsize=(5,5))
+ax = fig.add_subplot(111)
+t = np.arange(coef.shape[1]) * T
 
-fig, axs = plt.subplots(1, 1)
-pcm = axs.pcolormesh(x, scales_freq, coef)
-axs.set_yscale("log")
-axs.set_xlabel("Time (s)")
-axs.set_ylabel("Frequency (Hz)")
-axs.set_title("Continuous Wavelet Transform (Scaleogram)")
-fig.colorbar(pcm, ax=axs)
+ax.pcolormesh(t, scales_freq, data, cmap=cmap, vmin=data.min(), vmax=data.max(), shading='auto')
+
 plt.show()
+
+# (cA, cD) = pywt.dwt(data = y, wavelet = 'cmor1.5-1')
+
+print(pywt.wavelist(kind='discrete'))
