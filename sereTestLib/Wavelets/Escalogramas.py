8c4866cc0048c5a8604475a0dc76d88bbbce9314
DiegoBar2024 <diego.barboza@correo.ucu.edu.uy>
2025-02-06 19:29:08 -0300

    Avances en sincronizacion de pasos con nro de pasos como parametro

--- sereTestLib/Wavelets/Escalogramas.py
+++ sereTestLib/Wavelets/Escalogramas.py
@@ -3,6 +3,7 @@
 import sys
 from scipy.signal import *
 import numpy as np
+from matplotlib import pyplot as plt
 sys.path.append('C:/Yo/Tesis/SL2205-0.8/SL2205-0.8/sereTestLib/Cinematica')
 
 from SegmentacionM1 import *
@@ -34,8 +35,11 @@ matriz_segmentada = np.zeros(len(pasos), dtype = object)
 ## Creo un vector en donde voy guardando las extensiones en muestras correspondientes a cada paso
 extensiones_pasos = []
 
+## Especifico la cantidad de pasos que quiero representar en mis escalogramas
+cantidad_pasos = 5
+
 ## Ahora itero para cada uno de los pasos que tengo segmentados
-for i in range (len(pasos)):
+for i in range (1, len(pasos) - (cantidad_pasos - 1)):
 
     ## Le doy a la extensión un valor predefinido de 400 muestras (aproximadamente 2 segundos)
     extension = 400
@@ -49,12 +53,12 @@ for i in range (len(pasos)):
     ## Filas de <<matriz>>: Son cada uno de los 6 canales que tengo. Es decir señales de acelerómetros y giroscopios
     ## Columnas de <<matriz>>: Son cada uno de los instantes temporales que componen el paso detectado entre dos ICs
     ## Para mitigar los efectos de borde se le agrega una extensión en ambos lados del intervalo [IC[i] - e, IC[i+1] + e]
-    matriz = np.array([ acel[:,0][(pasos[i]['IC'][0] - extension) : (pasos[i]['IC'][1] + extension)],
-                        acel[:,1][(pasos[i]['IC'][0] - extension) : (pasos[i]['IC'][1] + extension)],
-                        acel[:,2][(pasos[i]['IC'][0] - extension) : (pasos[i]['IC'][1] + extension)],
-                        gyro[:,0][(pasos[i]['IC'][0] - extension) : (pasos[i]['IC'][1] + extension)],
-                        gyro[:,1][(pasos[i]['IC'][0] - extension) : (pasos[i]['IC'][1] + extension)],
-                        gyro[:,2][(pasos[i]['IC'][0] - extension) : (pasos[i]['IC'][1] + extension)] ])
+    matriz = np.array([ acel[:,0][(pasos[i]['IC'][0] - extension) : (pasos[i + (cantidad_pasos - 1)]['IC'][1] + extension)],
+                        acel[:,1][(pasos[i]['IC'][0] - extension) : (pasos[i + (cantidad_pasos - 1)]['IC'][1] + extension)],
+                        acel[:,2][(pasos[i]['IC'][0] - extension) : (pasos[i + (cantidad_pasos - 1)]['IC'][1] + extension)],
+                        gyro[:,0][(pasos[i]['IC'][0] - extension) : (pasos[i + (cantidad_pasos - 1)]['IC'][1] + extension)],
+                        gyro[:,1][(pasos[i]['IC'][0] - extension) : (pasos[i + (cantidad_pasos - 1)]['IC'][1] + extension)],
+                        gyro[:,2][(pasos[i]['IC'][0] - extension) : (pasos[i + (cantidad_pasos - 1)]['IC'][1] + extension)] ])
 
     ## Agrego la matriz al tensor de datos segmentados
     matriz_segmentada[i] = matriz
@@ -74,7 +78,7 @@ for i in range (len(pasos)):
 matriz_escalogramas = np.zeros(len(pasos), dtype = object)
 
 ## Itero para cada uno de los pasos que tengo segmentados
-for i in range (len(pasos)):
+for i in range (1, len(pasos) - (cantidad_pasos - 1)):
 
     ## Transformada de Wavelet de la aceleración en el eje x
     coef1, scales_freq = pywt.cwt(data = matriz_segmentada[i][0], scales = escalas, wavelet = wavelet, sampling_period = periodoMuestreo)
@@ -102,17 +106,23 @@ for i in range (len(pasos)):
                                             coef5[:,extensiones_pasos[i]: coef5.shape[1] - extensiones_pasos[i]], 
                                             coef6[:,extensiones_pasos[i]: coef6.shape[1] - extensiones_pasos[i]]     ])
 
-    ## Hago el remuestreo de la señal temporal para quue el tensor me quede de dimensión fija
-    matriz_escalogramas[i] = resample(matriz_escalogramas[i], 200, axis = 2)
+    # ## Hago el remuestreo de la señal temporal para quue el tensor me quede de dimensión fija
+    # matriz_escalogramas[i] = resample(matriz_escalogramas[i], 200, axis = 2)
+
+    ## Graficación de la señal en el tiempo
+    plt.plot(acel[:,2][pasos[i]['IC'][0] : pasos[i + (cantidad_pasos - 1)]['IC'][1]])
+    plt.show()
 
     ## Graficación del escalograma en el i-ésimo paso
-    data = np.abs(coef3[:,extensiones_pasos[i]: coef3.shape[1] - extensiones_pasos[i]]) ** 2
+    data = np.abs(coef3[:,extensiones_pasos[i]: coef3.shape[1] - extensiones_pasos[i]])
     cmap = plt.get_cmap('jet', 256)
     fig = plt.figure(figsize=(5,5))
     ax = fig.add_subplot(111)
     t = np.arange(coef3[:,extensiones_pasos[i]: coef3.shape[1] - extensiones_pasos[i]].shape[1]) * periodoMuestreo
     ax.pcolormesh(t, scales_freq, data, cmap=cmap, vmin=data.min(), vmax=data.max(), shading='auto')
+    # ax2 = fig.add_subplot(111)
+    # ax2.plot(acel[:,2][pasos[i - (cantidad_pasos - 1)]['IC'][0] : pasos[i + (cantidad_pasos - 1)]['IC'][1]])
     plt.xlabel("Tiempo (s)")
     plt.ylabel("Frecuencia (Hz)")
-    plt.title("$|CWT(t,f)|^2$")
+    plt.title("$|CWT(t,f)|$")
     plt.show()
